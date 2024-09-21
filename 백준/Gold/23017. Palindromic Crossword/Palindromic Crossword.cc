d4be838d5931bc44990beb12706b236a45e9ae8b
merin <101080932+yerinirey@users.noreply.github.com>
2024-09-21 20:05:41 +0900

    [Gold II] Title: Palindromic Crossword, Time: 428 ms, Memory: 4332 KB, Score: 28 point -BaekjoonHub

--- 백준/Gold/23017. Palindromic Crossword/Palindromic Crossword.cc
+++ 백준/Gold/23017. Palindromic Crossword/Palindromic Crossword.cc
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <queue>
 using namespace std; 
-char map[51][51];
+char map[1001][1001];
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int T; cin >> T;
